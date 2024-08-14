8053af9d5d7839d55398ebfb7ae6027e0d68e1ac
Esteban <estebanquintana6@gmail.com>
2024-08-14 00:39:30 -0600

    adding details to header

--- src/app/landing/Header.tsx
+++ src/app/landing/Header.tsx
@@ -4,7 +4,15 @@ export default function Header() {
     return (
         <div className="w-full h-screen bg-brown-100 flex">
             <div className="m-auto">
-                <h1 className="text-9xl text-primary">MUN TA NYA <br/> LAB</h1>
+                <div className="block relative">
+                    <h1 className="text-9xl text-primary">
+                        MUN TA NYA <br/>LAB
+                    </h1>
+                    <div className="flex absolute top-1/2 left-1/2 w-[40vh]">
+                        <span className="table-caption text-sm text-primary w-14 h-24">Estudio de diseño circular</span>
+                        <span className="text-sm text-primary ml-auto">est. 2024</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
