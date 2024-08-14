b59916432cd10dce59d6a292dd80cbc159cb7d9e
Esteban <estebanquintana6@gmail.com>
2024-08-14 01:02:44 -0600

    finished header

--- src/app/landing/Header.tsx
+++ src/app/landing/Header.tsx
@@ -5,10 +5,10 @@ export default function Header() {
         <div className="w-full h-screen bg-brown-100 flex px-6 sm:px-0">
             <div className="m-auto">
                 <div className="block sm:relative">
-                    <h1 className="text-9xl text-primary font-zodiak-light">
+                    <h1 className="text-8xl sm:text-9xl text-primary font-zodiak-light">
                         MUN TA NYA <br/>LAB
                     </h1>
-                    <div className="flex sm:absolute sm:top-1/2 sm:left-1/2 sm:w-[40vh]">
+                    <div className="flex mt-5 sm:mt-0 sm:absolute sm:top-1/2 sm:left-1/2 sm:w-[40vh]">
                         <span className="table-caption text-sm text-primary w-14 h-24">Estudio de diseño circular</span>
                         <span className="text-sm text-primary ml-auto">est. 2024</span>
                     </div>
