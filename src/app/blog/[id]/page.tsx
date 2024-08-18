d9cda42196253e95fe8497ffddfa7a652fb03ee2
Esteban <estebanquintana6@gmail.com>
2024-08-17 19:04:26 -0600

    blog entry page

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,10 @@
+import BlogEntry from "./BlogEntry";
+
+export default function BlogEntryPage() {
+    return (
+      <main>
+        <BlogEntry />
+      </main>
+    );
+}
+  
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <main className="flex">
+      <h2>En proceso</h2>
+    </main>
+  );
+}
