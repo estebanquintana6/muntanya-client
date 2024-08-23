5d4065275a7c0ac9f5e32a46f1b885cc07dced80
Esteban <estebanquintana6@gmail.com>
2024-08-22 20:32:11 -0600

    about improved

--- src/app/landing/About/VerticalAbout.tsx
+++ src/app/landing/About/VerticalAbout.tsx
@@ -10,12 +10,12 @@ export default function VerticalAbout() {
                     <div className="mt-auto mx-auto flex">
                         <h1 className="text-6xl font-zodiak-bold text-primary">Muntanya</h1>
                     </div>
-                    <div className="flex w-full mb-auto mt-3 p-8 sm:p-0">
+                    <div className="flex w-full mb-auto mt-8 p-8 sm:p-0">
                         <div className="w-1/2 sm:w-52 ml-auto mr-4">
-                            <h2 className="text-3xl text-primary ">Un estudio independiente de diseño</h2>
+                            <h2 className="text-2xl sm:text-3xl text-primary ">Un estudio independiente de diseño</h2>
                         </div>
                         <div className="h-full border-l-4 border-primary" />
-                        <h3 className="w-1/2 text-xl text-primary ml-4 mr-auto">Somos un estudio que hace x y z</h3>
+                        <h3 className="w-1/2 text-md sm:text-xl text-primary ml-4 mr-auto">Somos un estudio que hace x y z</h3>
                     </div>
                 </div>
             </div>
