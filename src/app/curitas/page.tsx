5d39f9a4a88d4a33dfd90f75995bc2ecbf3e9f01
Esteban <estebanquintana6@gmail.com>
2024-09-07 16:00:11 -0600

    curitas

---
+++ src/app/curitas/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../landing/shared/Navbar";
+
+export default function Proyectos() {
+  return (
+    <main className="flex flex-col min-h-screen bg-primary">
+      <div className="p-6 m-auto">
+        <img src="/curitas.png"></img>
+      </div>
+    </main>
+  );
+}
