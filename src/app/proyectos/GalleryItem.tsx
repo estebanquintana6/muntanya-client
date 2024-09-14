553948edbc1f27439e9ffd25702e7e8cdaf2f269
Esteban <estebanquintana6@gmail.com>
2024-09-14 14:31:21 -0600

    rounding project images

--- src/app/proyectos/GalleryItem.tsx
+++ src/app/proyectos/GalleryItem.tsx
@@ -12,7 +12,7 @@ export default function GalleryItem({ product }: OwnProps) {
   return (
     <article className="col-span-1 p-4 sm:p-0 w-full sm:w-[25vw]">
       <Link href={`proyectos/${_id}/`}>
-        <img src={photo_urls[0]} className="w-full" />
+        <img src={photo_urls[0]} className="w-full rounded-xl" />
       </Link>
       <div className="mt-4 w-full">
         <div className="my-4 flex flex-row flex-wrap w-full gap-2">
