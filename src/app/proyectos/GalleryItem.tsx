dc57e36945f13fa046ee04de6256be7d586bcb8d
Esteban <estebanquintana6@gmail.com>
2024-09-15 18:05:46 -0600

    fade in gallery product images

--- src/app/proyectos/GalleryItem.tsx
+++ src/app/proyectos/GalleryItem.tsx
@@ -1,19 +1,30 @@
 "use client";
+import { useRef } from "react";
 import { Product } from "../utils/interfaces/product";
 import Link from "next/link";
+import { useIsVisible } from "../utils/useIsVisible";
 
 interface OwnProps {
   product: Product;
 }
 
 export default function GalleryItem({ product }: OwnProps) {
-  const { _id, title, photo_urls, tags = [] } = product;
+  const imgRef = useRef<HTMLDivElement>(null);
+  const isVisible = useIsVisible(imgRef);
+
+  const { _id, title, photo_urls, tags = [], description } = product;
 
   return (
     <article className="col-span-1 p-4 sm:p-0 w-full sm:w-[25vw]">
-      <Link href={`proyectos/${_id}/`}>
-        <img src={photo_urls[0]} className="w-full rounded-xl" />
-      </Link>
+      <div
+        ref={imgRef}
+        className={`transition-opacity ease-in duration-700 ${isVisible ? "opacity-100" : "opacity-0"}`}
+      >
+        <Link href={`proyectos/${_id}/`}>
+          <img src={photo_urls[0]} className="w-full rounded-xl" />
+        </Link>
+        <div className="h-8 w-8 bg-blue-600 transition-opacity ease-in duration-700 opacity-100 hover:opacity-0"></div>
+      </div>
       <div className="mt-4 w-full">
         <div className="my-4 flex flex-row flex-wrap w-full gap-2">
           {tags.map((tag) => (
@@ -26,6 +37,9 @@ export default function GalleryItem({ product }: OwnProps) {
           ))}
         </div>
         <h1 className="text-3xl font-zodiak-bold text-primary">{title}</h1>
+        <p className="text-md font-zodiak-regular text-primary line-clamp-3">
+          {description}
+        </p>
       </div>
     </article>
   );
---
+++ src/app/utils/useIsVisible.ts
@@ -0,0 +1,18 @@
+import { MutableRefObject, useEffect, useState } from "react";
+
+export function useIsVisible(ref: MutableRefObject<any>) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting);
+    });
+
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
