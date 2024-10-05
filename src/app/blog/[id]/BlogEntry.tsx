dbd8650f3129a2955e0e42ceb235aba357cde907
Esteban <estebanquintana6@gmail.com>
2024-10-05 15:05:26 -0600

    responsive blog entry

--- src/app/blog/[id]/BlogEntry.tsx
+++ src/app/blog/[id]/BlogEntry.tsx
@@ -22,13 +22,12 @@ export default function BlogEntryPage({ blogEntry }: OwnProps) {
   };
 
   return (
-    <section className="gap-8 w-full md:h-screen px-4 sm:px-12 pb-12 sm:blog-grid">
-      <div className="flex flex-col mb-12 sm:mb-0">
-        <h1 className="uppercase text-4xl sm:blog-entry-title block break-words hyphens-auto pb-4 text-brown-100 font-zodiak-light">
+    <section className="gap-8 w-full md:h-screen sm:pl-12 pb-12 sm:blog-grid">
+      <div className="flex flex-col mb-12 sm:mb-0 px-6 sm:px-12">
+        <h1 className="uppercase text-5xl sm:blog-entry-title block break-words hyphens-auto pb-4 text-brown-100 font-zodiak-light">
           {title}
         </h1>
         <div className="flex border-b-2 border-brown-50 text-brown-100">
-          <span className="text-sm mr-auto block mt-2 break-words hyphens-auto font-zodiak-bold">Majo Gayou</span>
           <h2 className="text-sm ml-auto block mt-2 break-words hyphens-auto font-zodiak-bold">
             {new Date(created_at).toLocaleDateString("es-MX", dateOptions)}
           </h2>
@@ -37,7 +36,7 @@ export default function BlogEntryPage({ blogEntry }: OwnProps) {
           {subtitle}
         </h3>
       </div>
-      <div className="overflow-y-scroll flex flex-col sm:px-12">
+      <div className="overflow-y-scroll flex flex-col px-6 sm:px-12">
         {photo_urls.length > 1 ? (
           <Swiper
             slidesPerView={"auto"}
